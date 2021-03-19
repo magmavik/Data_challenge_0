f0ef3cbc688798d3f954e260d7f2751872a4743c
magmar68 <74837356+magmar68@users.noreply.github.com>
2021-03-19 09:46:11 +0100

    Update ANALISI.py

--- ANALISI.py
+++ ANALISI.py
@@ -20,23 +20,23 @@ start_time =  pd.to_datetime('2020-12-13').date()
 
 regioni = df['denominazione_regione'].tolist()
 regioni = list(dict.fromkeys(regioni))
-regione = regioni[1]
 
-df = df[df['data'] > start_time ]
+df = df[df['data'] > start_time ]  #restrain period 
 
 date = df['data'].tolist() #date list
 date = list(dict.fromkeys(date))
+
 data_regioni = dict()
 passings = dict() #passings from yellow to orange/red
-peaks = dict()
+peaks = dict()  # dictionary of peaks for each region
 
 for reg in regioni:
     val = df.loc[df['denominazione_regione'].str.contains(str(reg), flags = re.I, regex = True)]['totale_casi'].tolist()
     col = df.loc[df['denominazione_regione'].str.contains(str(reg), flags = re.I, regex = True)]['colore'].tolist()
 
-    for i in range(1, len(val))[::-1]:
+    for i in range(1, len(val))[::-1]: # convert cumulative cases in daily cases
         val[i] -= val[i-1]
-    for i in range(1, len(val)-2, 3):
+    for i in range(1, len(val)-2, 3): #mean over three days for reducing fluctuantion 
         trio = np.array([val[i], val[i+1], val[i+2]])
         m = np.mean(trio)
         val[i] = m
@@ -47,12 +47,13 @@ for reg in regioni:
     peaks[reg] = []
     for i in range(0, len(val)-1):
         if (col[i] == 'giallo') & ((col[i+1] == 'arancione') | (col[i+1] == 'rosso')):
-            passings[reg].append(i)
-
-    df_merge = pd.DataFrame (list(zip(date, val,col)), columns=['date','dati_giornalieri','colore'])
+            passings[reg].append(i) #date of switching colour
+    
+    #creating a data frame for each region with
+    df_merge = pd.DataFrame (list(zip(date, val,col)), columns=['date','dati_giornalieri','colore']) 
     data_regioni[reg] = df_merge
 
-    n = 5
+    n = 5 # io non capire
     # peaks[reg] = df_merge.loc[argrelextrema(df_merge['dati_giornalieri'].values, np.greater_equal, order=n)[0]].index.tolist[]
     # print(df_merge['max'].tolist())
     peaks[reg] = argrelextrema(df_merge['dati_giornalieri'].values, np.greater_equal, order=n)[0].tolist()
