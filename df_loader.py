3e33d87561a6270b8240a70724ee3ae4e29f15b1
magmar68 <74837356+magmar68@users.noreply.github.com>
2021-03-24 10:49:11 +0100

    Update df_loader.py

--- df_loader.py
+++ df_loader.py
@@ -1,36 +1,95 @@
 #   Program for loading COVID-19 dataframes from github.
 #
-#   Dev:    Luca Malucelli
+#   Dev:    Luca Malucelli  Lorenzo Magnoni
 #   Ctc:    luca.malucelli@studenti.unimi.it
 
 
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
+import os.path
+
 
 import glob
 from datetime import date, timedelta
 
+# path to "protezione civile italiana" github repository
+path = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province-'
+dropped_columns = ['codice_provincia', 'lat', 'long', 'note']
+file_OUT = 'merge.csv'
+COLOR = True
+
+
 sdate = date(2020,2,24) #start date
 edate = date.today() - timedelta(days=1) #end date
-#edate = date(2020,2, 27)
 
-path = 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province-'
+#======================================================================================
 
+#================function==============================================================
+def create_date_list( _sdate, _edate):
+    _list = []
+    if (_sdate > _edate):
+        print('ERROR: unexpected input in __function__')
+        return 
+    # create a list of date from start date to end date
+    for time in pd.date_range(sdate, edate, freq = 'd'):
+        _list.append(time.strftime("%Y%m%d"))
+    return _list
+
+def create_merge_data_frame(_datelist):
+    _data_frames = []
+    for i in datelist:
+        filename = path + i + '.csv'
+        df = pd.read_csv(filename)
+        _data_frames.append(df)
+    return _data_frames
+
+def merge_data_frame(_data_frames, _datelist):
+    for i in _datelist:
+        filename = path + i + '.csv'
+        df = pd.read_csv(filename)
+        _data_frames.append(df)
+    return _data_frames
+
+#=====================================================================================
 datelist = []
-for time in pd.date_range(sdate, edate, freq = 'd'):
-    datelist.append(time.strftime("%Y%m%d"))
+df = []
 
-data_frames = []
+new_file = True
 
-for i in datelist:
-    filename = path + i + '.csv'
-    df = pd.read_csv(filename)
-    data_frames.append(df)
+if(os.path.exists(file_OUT)):
+    df = pd.read_csv(file_OUT)
+    print( ' updating file ', file_OUT,' ....')
+    sdate = pd.to_datetime( df['data'].max())
+    print(type(sdate))
+    print(type(edate))
+    if sdate == edate:
+        print('file', file_OUT,'already updated')
+        new_file = False
+    else: 
+        datelist = create_date_list(sdate,edate)
+        df = merge_data_frame(df, datelist)
+        print( file, ' file_OUT ' , 'updated')
+else:
+    print(' file', file_OUT,'doesn\'t already exist. Creating one.... it will take a while')
+    datelist = create_date_list(sdate,edate)
+    df = create_merge_data_frame(datelist)
+    print(df)
 
-frame = pd.concat(data_frames, axis = 0, ignore_index = True)
+#=====================================================================================
 
-frame.sort_values(['data', 'denominazione_regione', 'denominazione_provincia'], ascending = True)
-frame = frame.drop(columns=['codice_provincia', 'lat', 'long', 'note'])
+if new_file:
+    frame = df
+    frame['data'] = pd.to_datetime(frame['data']).dt.date
+    frame.sort_values(['data', 'denominazione_regione', 'denominazione_provincia'], ascending = True)
+    frame = frame.drop(columns = dropped_columns)
 
-frame.to_csv('output.csv')
+    if COLOR:
+        color_frame = pd.read_csv('https://raw.githubusercontent.com/imcatta/restrizioni_regionali_covid/main/dataset.csv')
+        color_frame['data'] = pd.to_datetime(df['data']).dt.date
+        color_frame.loc[df['denominazione_regione'] == 'Provincia autonoma Bolzano', 'denominazione_regione'] = 'P.A. Bolzano'
+        color_frame.loc[df['denominazione_regione'] == 'Provincia autonoma Trento', 'denominazione_regione'] = 'P.A. Trento'
+
+        frame = pd.merge(frame, df, how="left", on=["data", "denominazione_regione"])
+
+    print(frame.head)
+    frame.to_csv(file_OUT)
